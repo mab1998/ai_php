8ddfecde8d0c0d9bd727891fa63503c703aaac81
BENNAOUI AMEUR <bennaoui.ameur@gmail.com>
2024-12-19 17:55:21 +0100

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickai';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'qa_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '4.5';
+$config['installed'] = '1';
+?>
